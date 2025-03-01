c2ba957ebf142ef50ce956db18a0109a20d14d96
monkeymoves <lukejjmaggs@gmail.com>
2025-03-01 21:16:26 +0000

    first branch test

--- futureapp/settings.py
+++ futureapp/settings.py
@@ -25,7 +25,6 @@ from dotenv import load_dotenv
 load_dotenv()  # reads .env file automatically
 
 SECRET_KEY = os.getenv('SECRET_KEY')
-# SECURITY WARNING: keep the secret key used in production secret!
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG', 'False') == 'True'
